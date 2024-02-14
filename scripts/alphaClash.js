472d77f6d1c82a47f8b6c26816dfb913bbf60a70
RonySorkerr <mdronysarkar33299@gmail.com>
2024-02-14 21:52:24 +0600

    i am a failure

--- scripts/alphaClash.js
+++ scripts/alphaClash.js
@@ -12,3 +12,51 @@ function continueGame (){
     setBackgroundColor(alphabet);
 }
 
+
+// keyboard event
+function keyboardHandler(keys) {
+    const playerPressedKey = keys.key;
+
+    const currentAlphabetElement = document.getElementById('currentAlphabet');
+    const currentAlphabet = currentAlphabetElement.innerText;
+    const expectedAlphabet = currentAlphabet.toLowerCase();
+
+    console.log('expected is ', playerPressedKey, expectedAlphabet);
+
+    if (playerPressedKey === expectedAlphabet) {
+
+        const currentScore = getTextElementById('currentScore');
+        console.log('current score is ' , currentScore);
+
+
+
+
+
+        // const scoreCard = document.getElementById('currentScore');
+        // const currentScore = scoreCard.innerText;
+        // const score = parseInt(currentScore);
+
+        // console.log('score is', score);
+
+        // const newScore = score + 1;
+        // scoreCard.innerText = newScore;
+
+
+
+        // removeBackgroundColor(playerPressedKey);
+        // continueGame();
+        // console.log('right key pressed');
+    }
+    else {
+        // const remainingChances = document.getElementById('remainingLife');
+        // const remainingChance = remainingChances.innerText;
+        // const chances = parseInt(remainingChance);
+
+        // const newChance = chances - 1;
+
+        // remainingChances.innerText = newChance;
+    }
+}
+
+// keypressed
+document.addEventListener('keyup', keyboardHandler);
--- scripts/utility.js
+++ scripts/utility.js
@@ -34,42 +34,10 @@ function removeBackgroundColor(elementId) {
     element.classList.remove('bg-amber-400')
 }
 
-// keyboard event
-function keyboardHandler(keys) {
-    const playerPressedKey = keys.key;
 
-    const currentAlphabetElement = document.getElementById('currentAlphabet');
-    const currentAlphabet = currentAlphabetElement.innerText;
-    const expectedAlphabet = currentAlphabet.toLowerCase();
-
-    console.log('expected is ', playerPressedKey, expectedAlphabet);
-
-    if (playerPressedKey === expectedAlphabet) {
-        const scoreCard = document.getElementById('currentScore');
-        const currentScore = scoreCard.innerText;
-        const score = parseInt(currentScore);
-
-        console.log('score is', score);
-
-        const newScore = score + 1;
-        scoreCard.innerText = newScore;
-
-
-
-        removeBackgroundColor(playerPressedKey);
-        continueGame();
-        console.log('right key pressed');
-    }
-    else {
-        const remainingChances = document.getElementById('remainingLife');
-        const remainingChance = remainingChances.innerText;
-        const chances = parseInt(remainingChance);
-
-        const newChance = chances - 1;
-
-        remainingChances.innerText = newChance;
-    }
+function getTextElementById(elementId){
+    const element = document.getElementById(elementId);
+    const elemenentValue = element.innerText;
+    const value = pareit(elemenentValue);
+    return value;
 }
-
-// keypressed
-document.addEventListener('keyup', keyboardHandler);
