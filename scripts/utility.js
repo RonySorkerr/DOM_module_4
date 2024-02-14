052014051eb8f7b90c3c878ad5293576948ae6e7
RonySorkerr <mdronysarkar33299@gmail.com>
2024-02-14 10:07:25 +0600

    random array added

--- scripts/utility.js
+++ scripts/utility.js
@@ -7,3 +7,11 @@ function showElementById(elementId){
     const element = document.getElementById(elementId);
     element.classList.remove('hidden')
 }
+
+function getRendomAlphabet (){
+    const randomAlphabets = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+    // get a random aplhabet from the array
+    
+
+}
