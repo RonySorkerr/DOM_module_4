d61169235f081aea0ff54f83a56bf1604eeb15b8
RonySorkerr <mdronysarkar33299@gmail.com>
2024-02-14 21:19:59 +0600

    score dynamically changing

--- scripts/utility.js
+++ scripts/utility.js
@@ -1,14 +1,14 @@
-function hideElementById(eleementId){
+function hideElementById(eleementId) {
     const element = document.getElementById(eleementId)
     element.classList.add('hidden')
 }
 
-function showElementById(elementId){
+function showElementById(elementId) {
     const element = document.getElementById(elementId);
     element.classList.remove('hidden')
 }
 
-function getRendomAlphabet (){
+function getRendomAlphabet() {
     const randomAlphabets = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
     // get a random aplhabet from the array
@@ -16,40 +16,51 @@ function getRendomAlphabet (){
     const index = Math.round(randomNumber);
 
     const alphabet = randomAlphabets[index];
-    console.log('alphabt to press : ' ,alphabet);
+    console.log('alphabt to press : ', alphabet);
 
     // we have to return the value cause the user will need it
     return alphabet;
 
 }
 
-function setBackgroundColor(elementId){
+function setBackgroundColor(elementId) {
     const element = document.getElementById(elementId);
     element.classList.add('bg-amber-400')
 
 }
 
-function removeBackgroundColor(elementId){
+function removeBackgroundColor(elementId) {
     const element = document.getElementById(elementId);
     element.classList.remove('bg-amber-400')
 }
 
 // keyboard event
-function keyboardHandler(keys){
+function keyboardHandler(keys) {
     const playerPressedKey = keys.key;
-    
-    const currentAlphabetElement =  document.getElementById('currentAlphabet');
+
+    const currentAlphabetElement = document.getElementById('currentAlphabet');
     const currentAlphabet = currentAlphabetElement.innerText;
     const expectedAlphabet = currentAlphabet.toLowerCase();
 
-    console.log('expected is ' , playerPressedKey,  expectedAlphabet);
+    console.log('expected is ', playerPressedKey, expectedAlphabet);
 
-    if(playerPressedKey === expectedAlphabet){
+    if (playerPressedKey === expectedAlphabet) {
+        const scoreCard = document.getElementById('currentScore');
+        const currentScore = scoreCard.innerText;
+        const score = parseInt(currentScore);
+
+        console.log('score is', score);
+
+        const newScore = score + 1;
+        scoreCard.innerText = newScore;
+
+
+
         removeBackgroundColor(playerPressedKey);
         continueGame();
         console.log('right key pressed');
     }
-    else{
+    else {
         console.log('you are wrong');
     }
 }
